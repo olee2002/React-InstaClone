a170488a26c89fd3bb4ecf49fa5368278ae35c6e
babylee2002 <babylee2002@gmail.com>
2018-01-10 19:03:04 -0500

    package-lock.json added n comments styled

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
+import styled from 'styled-components'
+
+
+const CommentContainer = styled.div`
+display: flex;
+justify-content: flex-start;
+align-items: center;
+img{
+  height: 20px;
+  width: 20px;
+  border-radius: 100%;
+  margin: 8px;
+}
+`
+
 
 class Comment extends Component {
   render() {
     const {comment} = this.props;
     console.log(comment);
     return (
-      <div>
+      <CommentContainer>
         <img src={comment.profile_pic}/>
         <p>
           <strong>{comment.username}</strong>
           {comment.text}</p>
-      </div>
+          </CommentContainer>
     );
   }
 }
